00148c9029ce95c15ff9bb79ff57c4826cbe621b
王慧勇 <weifeng19911024@163.com>
2017-01-31 19:59:19 +0800

    手势识别器

--- Swipes/ViewController.m
+++ Swipes/ViewController.m
@@ -15,37 +15,28 @@
 
 @end
 
-static CGFloat const kMinimumGestureLength = 25;		// 最小距离
-static CGFloat const kMaximumVariance = 5;			// 最大偏差
-
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UISwipeGestureRecognizer *vertical = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(reportVerticalSwipe:)];
+    vertical.direction = UISwipeGestureRecognizerDirectionUp | UISwipeGestureRecognizerDirectionDown;
+    [self.view addGestureRecognizer:vertical];
+    
+    UISwipeGestureRecognizer *horizontal = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(reportHorizontalSwipe:)];
+    horizontal.direction = UISwipeGestureRecognizerDirectionLeft | UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:horizontal];
 }
 
-#pragma mark - TouchHandling
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [touches anyObject];
-    self.gestureStartPoint = [touch locationInView:self.view];
+- (void)reportHorizontalSwipe:(UIGestureRecognizer *)recognizer {
+    self.label.text = @"Horizontal swipe detected";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.label.text = @"";});
 }
 
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [touches anyObject];
-    CGPoint currentPosition = [touch locationInView:self.view];
-    
-    CGFloat deltaX = fabs(self.gestureStartPoint.x - currentPosition.x);
-    CGFloat deltaY = fabs(self.gestureStartPoint.y - currentPosition.y);
-    
-    if (deltaX >= kMinimumGestureLength && deltaY <= kMaximumVariance) {
-        self.label.text = @"Horizontal swipe detected";
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.label.text = @"Swipe";});
-    } else if (deltaY >= kMinimumGestureLength && deltaX <= kMaximumVariance) {
-        self.label.text = @"Vertical swipe detected";
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.label.text = @"Swipe";});
-    }
+- (void)reportVerticalSwipe:(UIGestureRecognizer *)recognizer {
+    self.label.text = @"Vertical swipe detected";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.label.text = @"";});
 }
 
 @end
