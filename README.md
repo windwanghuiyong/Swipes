e804e2d52fadcede8ae54f61248438dbffbf6271
王慧勇 <weifeng19911024@163.com>
2017-01-31 19:42:05 +0800

    人工实现轻扫手势

---
+++ README.md
@@ -0,0 +1,15 @@
+# swipe
+
+## 描述
+
+1. 轻扫手势
+
+## 知识点
+
+1. 人工实现轻扫手势
+
+## 参考
+
+《Beginning iPhone Development with Swift: Exploring the iOS SDK》
+
+《精通iOS开发》第7版 18章 轻点, 触摸, 手势
--- Swipes/ViewController.m
+++ Swipes/ViewController.m
@@ -10,7 +10,13 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (nonatomic) CGPoint gestureStartPoint;
+
 @end
+
+static CGFloat const kMinimumGestureLength = 25;		// 最小距离
+static CGFloat const kMaximumVariance = 5;			// 最大偏差
 
 @implementation ViewController
 
@@ -19,11 +25,27 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark - TouchHandling
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    self.gestureStartPoint = [touch locationInView:self.view];
 }
 
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint currentPosition = [touch locationInView:self.view];
+    
+    CGFloat deltaX = fabs(self.gestureStartPoint.x - currentPosition.x);
+    CGFloat deltaY = fabs(self.gestureStartPoint.y - currentPosition.y);
+    
+    if (deltaX >= kMinimumGestureLength && deltaY <= kMaximumVariance) {
+        self.label.text = @"Horizontal swipe detected";
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.label.text = @"Swipe";});
+    } else if (deltaY >= kMinimumGestureLength && deltaX <= kMaximumVariance) {
+        self.label.text = @"Vertical swipe detected";
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.label.text = @"Swipe";});
+    }
+}
 
 @end
